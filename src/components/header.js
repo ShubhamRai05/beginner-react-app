461217ce574f5ed15e44dcaa6a43cefe039a8bae
Shubham Rai <shubham56237@gmail.com>
2023-08-29 23:06:18 +0530

    updating title

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,42 @@
-const title = <h1 className="logo">FoodMania</h1>
+import { useState } from "react"
 
 
-const HeaderComponent = function () {
-    return (
-        <div className="header">
-            {title}
-            <ul className="nav-list">
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
-        </div>
+const title = () => {
+  const [title, setTitle] = useState("FOODMANIA")
+  return(   
+    <>      
+      <h1 className="logo">{title}</h1>
+     
+    </>
+ 
+  ) 
+
+}
  
 
-    )
+const HeaderComponent = function () {
+
+  const [authentication, setAuthentication] = useState(true)
+
+
+  return (
+    <div className="header">
+      {title()}
+      <ul className="nav-list">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+      {authentication ? <button onClick={() => {
+        setAuthentication(false)
+      }}>Login</button> : <button onClick={() => {
+        setAuthentication(true)
+      }}>Logout</button>}
+    </div>
+
+
+  )
 }
 
 export default HeaderComponent
