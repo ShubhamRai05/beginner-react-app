36c7e1d0a3eaf1b12d03441733bf18e2e1ebdf46
Shubham Rai <shubham56237@gmail.com>
2023-09-15 21:48:53 +0530

    yup

--- src/components/schemas/signUpSchema.js
+++ src/components/schemas/signUpSchema.js
@@ -3,7 +3,7 @@ import * as Yup from "yup"
 // yup is used for importing errors with respect to formik
 
 export const signUpSchema = Yup.object({
-    firstName:Yup.string().min(4).max(15).required("Please enter your name"),
+    firstName:Yup.string().min(4).max(15).required("Please enter your name as per specified format"),
     lastName: Yup.string().min(2).max(15).required("Please follow the specific format"),
     email : Yup.string().email().required("Invalid Email Id"),
     password:Yup.string().max(15).min(8).required(),
