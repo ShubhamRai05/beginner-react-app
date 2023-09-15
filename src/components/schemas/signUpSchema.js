9bb5f9f830147ab0332797886c6af8b0cab2714a
Shubham Rai <shubham56237@gmail.com>
2023-09-15 21:47:42 +0530

    yup

--- src/components/schemas/signUpSchema.js
+++ src/components/schemas/signUpSchema.js
@@ -5,7 +5,7 @@ import * as Yup from "yup"
 export const signUpSchema = Yup.object({
     firstName:Yup.string().min(4).max(15).required("Please enter your name"),
     lastName: Yup.string().min(2).max(15).required("Please write your lastname"),
-    email : Yup.string().email().required("Please write the correct email"),
+    email : Yup.string().email().required("Invalid Email Id"),
     password:Yup.string().max(15).min(8).required(),
     confirmPassword:Yup.string().oneOf([Yup.ref("password") , null] , "Password Does Not Match")
 })
